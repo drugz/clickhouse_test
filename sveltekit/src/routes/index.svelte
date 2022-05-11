96f9c6bca056d51570aef6bf0e4bae33815a0315
Oleg Korzhanov <ok@xfactorial.com>
2022-05-11 16:41:44 +0400

    index ui for create db

--- sveltekit/src/routes/index.svelte
+++ sveltekit/src/routes/index.svelte
@@ -10,3 +10,36 @@
 		};
 	};
 </script>
+
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	export let ping: string;
+	let status_create_database = '';
+	async function createClickhouseDB(): Promise<void> {
+		const response = await fetch('/api/create_db');
+		let body = await response.json();
+		status_create_database = body.message || 'Sorry. Some error';
+	}
+
+</script>
+
+<h1>Demo app</h1>
+<h2>SvelteKit & ClickHouse DB</h2>
+<hr />
+{#if ping}
+	<status transition:fade>
+		{ping}
+	</status>
+
+	<br />
+	<button on:click={createClickhouseDB}>Create database</button>
+	{#if status_create_database}
+		<status transition:fade>
+			{status_create_database}
+		</status>
+	{/if}
+	<br />
+{:else}
+	<status transition:fade>No connection to ClickHouse</status>
+{/if}
+
