2ec0dfa4d46cda9d5ba13141da845cfe7104eb5a
Oleg Korzhanov <ok@xfactorial.com>
2022-05-12 00:42:13 +0400

    cart_products change type for amount

--- sveltekit/src/routes/api/create_table.js
+++ sveltekit/src/routes/api/create_table.js
@@ -4,9 +4,9 @@ export async function get() {
     // -- products: id, price, no_discount_flag (bool)
     // -- cart_products: id, product_id, amount
     // -- coupons: id, discount_value (% of product price)
-    const queries = [
+   const queries = [
         "CREATE TABLE IF NOT EXISTS products (id UUID NOT NULL, price Decimal(9,2), nodiscount_flag BOOL) ENGINE = GenerateRandom(1, 5, 3)",
-        "CREATE TABLE IF NOT EXISTS cart_products (id UUID NOT NULL, product_id UUID, amount Int16) ENGINE = Memory",
+        "CREATE TABLE IF NOT EXISTS cart_products (id UUID NOT NULL, product_id UUID, amount TINYINT) ENGINE = Memory",
         "CREATE TABLE IF NOT EXISTS coupons (id UUID NOT NULL, discount_value TINYINT) ENGINE = GenerateRandom(1, 5, 3)",
     ];
     try {
