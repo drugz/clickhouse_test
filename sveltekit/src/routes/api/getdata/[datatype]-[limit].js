c2a7a2a294103180f8d036650f3cdf40f34040f7
Oleg Korzhanov <ok@xfactorial.com>
2022-05-14 02:29:07 +0400

    remove unnecessary awaits

--- sveltekit/src/routes/api/getdata/[datatype]-[limit].js
+++ sveltekit/src/routes/api/getdata/[datatype]-[limit].js
@@ -7,14 +7,14 @@ export async function get({ params }) {
 	try {
 		const handlers = {
 			products: async () => {
-				return await clickhouse.queryPromise(`select * from products limit ${limit}`);
+				return clickhouse.queryPromise(`select * from products limit ${limit}`);
 			},
 			coupons: async () => {
 				// get some limit number of coupones from randomed table, which less than 30%
-				return await clickhouse.queryPromise(`SELECT coupons.id, min2(abs(coupons.discount_value)/10,30) AS discount_value FROM coupons limit ${limit}`);
+				return clickhouse.queryPromise(`SELECT coupons.id, min2(abs(coupons.discount_value)/10,30) AS discount_value FROM coupons limit ${limit}`);
 			},
 			cart_products: async () => {
-				return await clickhouse.queryPromise(`SELECT cart_products.id, cart_products.amount, cart_products.product_id, products.price, products.no_discount_flag FROM cart_products INNER JOIN (SELECT * FROM products limit 1000) AS products ON (cart_products.product_id = products.id)`);
+				return clickhouse.queryPromise(`SELECT cart_products.id, cart_products.amount, cart_products.product_id, products.price, products.no_discount_flag FROM cart_products INNER JOIN (SELECT * FROM products limit 1000) AS products ON (cart_products.product_id = products.id)`);
 			},
 			total_cart_cost: async () => {
 				// WITH (SELECT min2(SUM(discount),100) AS discount FROM (SELECT min2(abs(coupons.discount_value)/10,30) AS discount FROM coupons limit 5 ) as c) AS discount 
@@ -30,7 +30,7 @@ export async function get({ params }) {
 				// FROM (SELECT * FROM cart_products) AS cart 
 				// INNER JOIN 
 				// (SELECT * FROM products limit 100) AS p ON (p.id=cart.product_id)
-				return await clickhouse.queryPromise(`WITH (SELECT min2(SUM(d),100) AS d FROM (SELECT min2(abs(coupons.discount_value)/10,30) AS d FROM coupons limit ${limit} ) as c) AS discount SELECT SUM(no_discount_flag!=true ? p.price*cart.amount : p.price*cart.amount*0.01*(100-discount)) AS total_cost, total_cost*1.1 as taxed_cost FROM (SELECT * FROM cart_products) AS cart INNER JOIN (SELECT * FROM products limit 100) AS p ON (p.id=cart.product_id)`)
+				return clickhouse.queryPromise(`WITH (SELECT min2(SUM(d),100) AS d FROM (SELECT min2(abs(coupons.discount_value)/10,30) AS d FROM coupons limit ${limit} ) as c) AS discount SELECT SUM(no_discount_flag!=true ? p.price*cart.amount : p.price*cart.amount*0.01*(100-discount)) AS total_cost, total_cost*1.1 as taxed_cost FROM (SELECT * FROM cart_products) AS cart INNER JOIN (SELECT * FROM products limit 100) AS p ON (p.id=cart.product_id)`)
 			},
 			default: ['no data']
 		};
