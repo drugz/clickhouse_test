405f4d6ca522b51c6eebd1b51f5b095412bbea9b
Oleg Korzhanov <ok@xfactorial.com>
2022-05-13 18:20:27 +0400

    lint

--- sveltekit/src/routes/api/ping.js
+++ sveltekit/src/routes/api/ping.js
@@ -1,24 +1,23 @@
 import clickhouse from '$lib/clickhouse.js';
 
 export async function get() {
+	const ping = await clickhouse.ping();
 
-    const ping = await clickhouse.ping();
-
-    if (ping) {
-        return {
-            status: 200,
-            body: {
-                message: "ClickHouse is alive",
-                result: true
-            }
-        }
-    } else {
-        return {
-            status: 500,
-            body: {
-                message: "Could not connect to ClickHouse",
-                result: false
-            }
-        }
-    }
+	if (ping) {
+		return {
+			status: 200,
+			body: {
+				message: 'ClickHouse is alive',
+				result: true
+			}
+		};
+	} else {
+		return {
+			status: 500,
+			body: {
+				message: 'Could not connect to ClickHouse',
+				result: false
+			}
+		};
+	}
 }
