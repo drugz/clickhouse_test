a7c50cee61cdb991b3929009845acec14bd9f08b
Oleg Korzhanov <ok@xfactorial.com>
2022-05-14 06:39:43 +0400

    add process.env.CLICKHOUSE_HOST

--- sveltekit/src/lib/clickhouse.js
+++ sveltekit/src/lib/clickhouse.js
@@ -1,7 +1,12 @@
 import { ClickHouseClient } from '@depyronick/clickhouse-client';
+// import { config } from "dotenv";
+// config({ path: process.env });
 
+// dotenv.config();
+console.log("process.env:", process.env);
+console.log("process.env.CLICKHOUSE_HOST:", process.env.CLICKHOUSE_HOST);
 const clickhouse = new ClickHouseClient({
-	host: '127.0.0.1',
+	host: process.env.CLICKHOUSE_HOST||'127.0.0.1',
 	port: 8123,
 	username: 'default',
 	database: 'default',
